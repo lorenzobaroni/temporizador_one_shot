e4e6a4ea27076e5ea84b1e458917ea7747b9c3f1
Lorenzo Baroni <lorenzob18@hotmail.com>
2025-01-30 15:42:29 -0300

    Criação e inicialização das gpio's

--- temporizador_one_shot.c
+++ temporizador_one_shot.c
@@ -1,14 +1,39 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
+#include "hardware/gpio.h"
+#include "hardware/timer.h"
 
+// Definições de pinos
+#define LED_AZUL  11
+#define LED_VERMELHO 12
+#define LED_VERDE 13
+#define BOTAO 5
 
+// Variável para controlar se o botão pode ser pressionado novamente
+volatile bool pode_pressionar = true;
 
-int main()
-{
+int main() {
     stdio_init_all();
 
+   
+    gpio_init(LED_AZUL);
+    gpio_set_dir(LED_AZUL, GPIO_OUT);
+    gpio_put(LED_AZUL, 0); 
+
+    gpio_init(LED_VERMELHO);
+    gpio_set_dir(LED_VERMELHO, GPIO_OUT);
+    gpio_put(LED_VERMELHO, 0); 
+
+    gpio_init(LED_VERDE);
+    gpio_set_dir(LED_VERDE, GPIO_OUT);
+    gpio_put(LED_VERDE, 0); 
+
+  
+    gpio_init(BOTAO);
+    gpio_set_dir(BOTAO, GPIO_IN);
+    gpio_pull_up(BOTAO);
+
     while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
+        sleep_ms(100); // Aguarda para evitar consumo excessivo da CPU
     }
 }
